6b9479a78733eacd338e3cdbaf05a28c21180aff
Oliver Dippel <o.dippel@gmx.de>
2024-01-30 19:42:34 +0100

    fixing cst pins

--- riocore/generator/pins/cst/pins.py
+++ riocore/generator/pins/cst/pins.py
@@ -7,11 +7,11 @@ class Pins:
         for pname, pins in self.config["pinlists"].items():
             data.append(f"// ### {pname} ###")
             for pin, pin_config in pins.items():
-                data.append(f"IO_LOC \"{pin_config['pin']}\" {pin_config['varname']};")
+                data.append(f"IO_LOC \"{pin_config['varname']}\" {pin_config['pin']};")
                 if pin_config.get("pullup", False):
-                    data.append(f"IO_PORT \"{pin_config['pin']}\" IO_TYPE=LVCMOS33 PULL_MODE=UP;")
+                    data.append(f"IO_PORT \"{pin_config['varname']}\" IO_TYPE=LVCMOS33 PULL_MODE=UP;")
                 else:
-                    data.append(f"IO_PORT \"{pin_config['pin']}\" IO_TYPE=LVCMOS33;")
+                    data.append(f"IO_PORT \"{pin_config['varname']}\" IO_TYPE=LVCMOS33;")
             data.append("")
         data.append("")
         open(f"{path}/pins.cst", "w").write("\n".join(data))
