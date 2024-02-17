660559e55ca886418423ec056074cff4562433dc
Oliver Dippel <o.dippel@gmx.de>
2024-02-18 00:09:33 +0100

    fixing tx_frame

--- riocore/__init__.py
+++ riocore/__init__.py
@@ -551,7 +551,7 @@ class Project:
                 byte_start, byte_size, bit_offset = self.get_bype_pos(output_pos, variable_size)
                 byte_start = self.buffer_bytes - 1 - byte_start
                 if plugin_instance.TYPE == "frameio":
-                    txdata[byte_start - (byte_size - 1) : byte_start + 1] = value
+                    txdata[byte_start - (byte_size - 1) : byte_start + 1] = value[0:byte_size]
                 elif variable_size > 1:
                     txdata[byte_start - (byte_size - 1) : byte_start + 1] = list(pack("<i", int(value)))[0:byte_size]
                 else:
@@ -608,7 +608,7 @@ class Project:
                 byte_start = self.buffer_bytes - 1 - byte_start
 
                 if plugin_instance.TYPE == "frameio":
-                    value = rxdata[byte_start - (byte_size - 1) : byte_start + 1]
+                    value = rxdata[byte_start - (byte_size - 1) : byte_start + 1][0:byte_size]
                     # print(value)
                 elif variable_size > 1:
                     byte_pack = rxdata[byte_start - (byte_size - 1) : byte_start + 1]
