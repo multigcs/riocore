fced637967a9eb0a4bf063ea17caed2ad25e6b3c
Oliver Dippel <odippel@ypsilon.net>
2024-02-26 11:15:05 +0100

    fixing camera offsets

--- riocore/generator/LinuxCNC.py
+++ riocore/generator/LinuxCNC.py
@@ -263,16 +263,24 @@ class LinuxCNC:
                 camera_device = camera.get("device", f"/dev/video{camera_num}")
                 tabname = camera.get("tabname", f"Camera-{camera_num}")
                 tablocation = camera.get("tablocation", "Pyngcgui")
-                offsets = camera.get("offset")
+                offsets = camera.get("offset", {})
                 ini_setup["DISPLAY"][f"EMBED_TAB_NAME|CAM{camera_num}"] = tabname
                 if gui != "axis":
                     ini_setup["DISPLAY"][f"EMBED_TAB_LOCATION|CAM{camera_num}"] = tablocation
                 ini_setup["DISPLAY"][
                     f"EMBED_TAB_COMMAND|CAM{camera_num}"
                 ] = f"mplayer -wid {{XID}} tv:// -tv driver=v4l2:device={camera_device} -vf rectangle=-1:2:-1:240,rectangle=2:-1:320:-1 -really-quiet"
-                if offsets and offset_num == 0:
+                if not offsets and offset_num == 0:
+                    offsets = {}
+                    for axis_name, joints in self.axis_dict.items():
+                        offsets[axis_name] = 0
+                
+                if offset_num == 0:
                     mdi_command = ["G92"]
                     for axis_name, joints in self.axis_dict.items():
+                        
+                        print(self.axis_dict)
+                        
                         diff = 0
                         if axis_name in offsets:
                             diff = offsets[axis_name]
