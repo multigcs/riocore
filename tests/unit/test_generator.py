4f97f69ba144fb06c47be8e464ddd16d8f8747e9
Oliver Dippel <o.dippel@gmx.de>
2024-08-16 13:39:45 +0200

    adding unit tests: generate all config.json files / activate pll

--- tests/unit/test_generator.py
+++ tests/unit/test_generator.py
@@ -2,6 +2,8 @@
 #
 #
 
+import glob
+import os
 import pytest
 import riocore
 import os.path
@@ -33,4 +35,35 @@ def test_generator(config, target, protocol):
             assert False
         rio_v = open(f"tests/unit/output/{target}/Gateware/rio.v", "r").read()
         if "spi #(" not in rio_v:
+            assert False
+
+
+def test_generade_all():
+    for config in glob.glob("riocore/configs/*/config.json"):
+        print("###################")
+        print(config)
+        print("###################")
+        # clean output directory
+        if os.path.exists("tests/unit/output"):
+            shutil.rmtree("tests/unit/output")
+
+        # load project
+        project = riocore.Project(config, "tests/unit/output")
+
+        # force pll generator
+        project.config["osc"] = 12000000
+        project.config["osc_clock"] = 12000000
+        project.config["speed"] = 24000000
+
+        # generate
+        project.generator(False)
+        print("")
+
+        if not os.path.exists(f"tests/unit/output/{project.config['name']}/Gateware/rio.v"):
+            assert False
+
+        if not os.path.exists(f"tests/unit/output/{project.config['name']}/LinuxCNC/rio.hal"):
+            assert False
+
+        if not os.path.exists(f"tests/unit/output/{project.config['name']}/LinuxCNC/rio.ini"):
             assert False
