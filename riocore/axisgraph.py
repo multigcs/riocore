7ca497d5a0807a82a73efd616d4bba981a8a84d5
Oliver Dippel <o.dippel@gmx.de>
2024-11-12 09:26:38 +0100

    updating axis-graph

--- riocore/axisgraph.py
+++ riocore/axisgraph.py
@@ -12,33 +12,44 @@ class AxisGraph:
     def svg(self):
         try:
             ja_links = {}
+            last_section = None
             last_axis = None
             last_jnum = None
             axis = {}
             joints = {}
+            kins = None
             for line in self.ini_data.split("\n"):
-                if line.startswith("[AXIS_"):
-                    last_axis = line.strip()[:-1].split("_")[1]
-                    last_jnum = None
-                    axis[last_axis] = {
-                        "info": [],
-                    }
-                elif line.startswith("[JOINT_"):
-                    last_jnum = line.strip()[:-1].split("_")[1]
-                    joints[last_jnum] = {
-                        "info": [],
-                    }
-                    ja_links[f"joint-{last_jnum}"] = (f"axis-{last_axis}", "")
+                if line.startswith("#"):
+                    continue
+                elif line.startswith("["):
+                    last_section = line.split("[")[1].split("]")[0]
+                    if last_section.startswith("AXIS_"):
+                        last_axis = last_section.split("_")[1]
+                        last_jnum = None
+                        axis[last_axis] = {
+                            "info": [],
+                        }
+                    elif last_section.startswith("JOINT_"):
+                        last_jnum = last_section.split("_")[1]
+                        joints[last_jnum] = {
+                            "info": [],
+                        }
+                        ja_links[f"joint-{last_jnum}"] = (f"axis-{last_axis}", "")
+                    else:
+                        last_axis = None
+                        last_jnum = None
 
-                elif (line.startswith("MIN_") or line.startswith("MAX_")) and not last_jnum and last_axis:
-                    key = line.split("=")[0].strip()
-                    value = line.split("=")[-1].strip()
-                    axis[last_axis]["info"].append(f"{key}={value}")
-
-                elif line.startswith("HOME") and last_jnum:
-                    key = line.split("=")[0].strip()
-                    value = line.split("=")[-1].strip()
-                    joints[last_jnum]["info"].append(f"{key}={value}")
+                elif "=" in line:
+                    parts = line.split("=", 1)
+                    key = parts[0].strip()
+                    value = parts[1].strip()
+                    if last_axis and not last_jnum:
+                        axis[last_axis]["info"].append(f"{key}={value}")
+                    elif last_jnum:
+                        if key in {"TYPE", "MIN_LIMIT", "MAX_LIMIT", "SCALE_OUT", "SCALE_IN", "HOME_SEQUENCE"}:
+                            joints[last_jnum]["info"].append(f"{key}={value}")
+                    elif last_section == "KINS" and key == "KINEMATICS":
+                        kins = value
 
             for line in self.hal_data.split("\n"):
                 if line.startswith("net joint-") and "-home-sw-in <= " in line:
@@ -72,6 +83,20 @@ class AxisGraph:
 
             for joint, target in ja_links.items():
                 self.gAll.edge(joint, target[0], label=target[1], color="white", fontcolor="white")
+
+            if kins:
+                infos = "|".join(kins.split())
+                label = f"{{{{Kins|{infos}}}}}"
+                self.gAll.node(
+                    f"kins",
+                    shape="record",
+                    label=label,
+                    fontsize="11pt",
+                    style="rounded, filled",
+                    fillcolor="lightblue",
+                )
+                for name in axis:
+                    self.gAll.edge(f"axis-{name}", "kins", color="white", fontcolor="white")
 
             return self.gAll.pipe()
 
