38ab1b4b5f09f318cedb4d85575c7cfc2e17a058
Oliver Dippel <o.dippel@gmx.de>
2024-12-09 16:49:42 +0100

    readding IO_TYPE to cst pins (only if not set to default)

--- riocore/generator/pins/cst/pins.py
+++ riocore/generator/pins/cst/pins.py
@@ -11,21 +11,21 @@ class Pins:
             data.append(f"// ### {pname} ###")
             for pin, pin_config in pins.items():
                 data.append(f"IO_LOC \"{pin_config['varname']}\" {pin_config['pin']};")
-
                 drive = pin_config.get("drive", "4")
-                # iostandard = pin_config.get("iostandard", "LVCMOS33").upper()
-                # slew = pin_config.get("slew", "SLOW").upper()
+                iostandard = pin_config.get("iostandard", "").upper()
+                io_type = ""
+                if iostandard and iostandard != "LVTTL":
+                    io_type = f" IO_TYPE={iostandard}"
 
                 if pin_config["direction"] == "input":
                     if pin_config.get("pullup", False) or pin_config.get("pull") == "up":
-                        data.append(f"IO_PORT \"{pin_config['varname']}\" PULL_MODE=UP;")
+                        data.append(f"IO_PORT \"{pin_config['varname']}\" PULL_MODE=UP{io_type};")
                     elif pin_config.get("pulldown", False) or pin_config.get("pull") == "down":
-                        data.append(f"IO_PORT \"{pin_config['varname']}\" PULL_MODE=DOWN;")
-                    # else:
-                    #     data.append(f"IO_PORT \"{pin_config['varname']}\" IO_TYPE={iostandard};")
+                        data.append(f"IO_PORT \"{pin_config['varname']}\" PULL_MODE=DOWN{io_type};")
+                    elif io_type:
+                        data.append(f"IO_PORT \"{pin_config['varname']}\"{io_type};")
                 else:
-                    data.append(f"IO_PORT \"{pin_config['varname']}\" DRIVE={drive};")
-                    # IO_TYPE={iostandard}
+                    data.append(f"IO_PORT \"{pin_config['varname']}\" DRIVE={drive}{io_type};")
 
             data.append("")
         data.append("")
