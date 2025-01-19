ed515f7cc207f45c9fbb4ef1e36cfd151955e476
Oliver <oliver@office-station.local>
2025-01-19 14:05:47 -0500

    fixing joypad-addon

--- riocore/generator/addons/joypad/linuxcnc.py
+++ riocore/generator/addons/joypad/linuxcnc.py
@@ -1,10 +1,8 @@
 def hal(parent):
-    output = []
-
     linuxcnc_config = parent.project.config["jdata"].get("linuxcnc", {})
     joypad = linuxcnc_config.get("joypad", {})
     if not joypad or not joypad.get("enable"):
-        return output
+        return []
 
     joypad_type = joypad.get("name")
     joypad_btn_slow = joypad.get("slow")
@@ -17,18 +15,17 @@ def hal(parent):
         axis_lower = axis_name.lower()
         muxes.append(f"mux2_{axis_lower}")
 
-    output = []
-    output.append(f"loadusr -W hal_input -KRAL {joypad_type}")
-    output.append("loadrt mux4 names=joy_mux4")
-    output.append(f"loadrt mux2 names={','.join(muxes)}")
-    output.append("")
-    output.append("addf joy_mux4 servo-thread")
-    output.append("")
-    output.append("setp joy_mux4.in0 0.0    # Setting this input to 0 prevents motion unless one of the other buttons is pressed.")
-    output.append("setp joy_mux4.in1 50.0   # Max jog speed when first speed select button is pressed.")
-    output.append("setp joy_mux4.in2 500.0  # Max jog speed when second speed select button is pressed.")
-    output.append("setp joy_mux4.in3 2000.0 # Max jog speed when third speed select button is pressed.")
-    output.append("")
+    parent.halg.fmt_add(f"loadusr -W hal_input -KRAL {joypad_type}")
+    parent.halg.fmt_add("loadrt mux4 names=joy_mux4")
+    parent.halg.fmt_add(f"loadrt mux2 names={','.join(muxes)}")
+    parent.halg.fmt_add("")
+    parent.halg.fmt_add("addf joy_mux4 servo-thread")
+    parent.halg.fmt_add("")
+    parent.halg.fmt_add("setp joy_mux4.in0 0.0    # Setting this input to 0 prevents motion unless one of the other buttons is pressed.")
+    parent.halg.fmt_add("setp joy_mux4.in1 50.0   # Max jog speed when first speed select button is pressed.")
+    parent.halg.fmt_add("setp joy_mux4.in2 500.0  # Max jog speed when second speed select button is pressed.")
+    parent.halg.fmt_add("setp joy_mux4.in3 2000.0 # Max jog speed when third speed select button is pressed.")
+    parent.halg.fmt_add("")
 
     joy_mux4_sel0 = []
     joy_mux4_sel1 = []
@@ -65,13 +62,13 @@ def hal(parent):
             if jaxis[0] == "-":
                 jaxis = jaxis[1:]
                 reverse = True
-            output.append(f"# {axis_name}-Axis")
+            parent.halg.fmt_add(f"# {axis_name}-Axis")
             if reverse:
-                output.append(f"setp input.0.{jaxis}-scale -127.5")
-            output.append(f"addf mux2_{axis_lower} servo-thread")
+                parent.halg.fmt_add(f"setp input.0.{jaxis}-scale -127.5")
+            parent.halg.fmt_add(f"addf mux2_{axis_lower} servo-thread")
             parent.halg.net_add("halui.machine.is-on", f"mux2_{axis_lower}.sel")
             parent.halg.net_add(f"input.0.{jaxis}-position", f"mux2_{axis_lower}.in1")
             parent.halg.net_add(f"mux2_{axis_lower}.out", f"halui.axis.{axis_lower}.analog")
             for joint, joint_setup in joints.items():
                 parent.halg.net_add(f"mux2_{axis_lower}.out", f"halui.joint.{joint}.analog")
-    return output
+    return []
