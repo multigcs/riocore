de8ba3f63eb90655d9e3c24ca316df0ead14700c
Oliver <oliver@office-station.local>
2025-01-21 17:18:54 +0100

    update readmeas

--- riocore/plugins/i2cbus/plugin.py
+++ riocore/plugins/i2cbus/plugin.py
@@ -80,10 +80,11 @@ graph LR;
         self.MAX_DIN = 48
 
         self.DESCRIPTION += "\n\nDevices:\n"
+        self.DESCRIPTION += "| Image | Name |\n"
+        self.DESCRIPTION += "| --- | --- |\n"
         for device_path in sorted(glob.glob(os.path.join(plugin_path, "devices", "*", "__init__.py"))):
             device_name = os.path.basename(os.path.dirname(device_path))
-
-            self.DESCRIPTION += f"* [{device_name}](devices/{device_name}/) <img src=\"devices/{device_name}/image.png\" height=\"24\">\n"
+            self.DESCRIPTION += f"| <img src=\"devices/{device_name}/image.png\" height=\"24\"> | [{device_name}](devices/{device_name}/) |\n"
 
         if not self.devices:
             return
