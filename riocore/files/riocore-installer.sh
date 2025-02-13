c2962edf582ad71965bc4d05a12ee78327bcca27
Oliver <oliver@office-station.local>
2025-02-13 20:36:33 +0100

    update installer-script

--- riocore/files/riocore-installer.sh
+++ riocore/files/riocore-installer.sh
@@ -4,9 +4,17 @@
 
 PWD="`pwd`"
 SYSTEM="arm64"
+SYSTEM2="arm64"
 if test "`uname -m`" = "x86_64"
 then
 	SYSTEM="x64"
+	SYSTEM2="amd64"
+fi
+
+TARGETDIR=~
+if test "$1" != "" && test -d "$1"
+then
+	TARGETDIR="$1"
 fi
 
 TEMPFILE="`mktemp /tmp/rioXXXXXX.tmp`"
@@ -14,41 +22,64 @@ TC_ICESTORM=""
 TC_GOWIN=""
 
 doexit() {
-	rm -rf $TEMPFILE
+	rm -rf ${TEMPFILE} ${TEMPFILE}2
 	exit $1
 }
 
-cd ~ || doexit 1
+cd $TARGETDIR || doexit 1
 
-
-if test "$SYSTEM" = "x64"
+echo "whiptail --checklist \"what do you want to install ?\\n Target-Directory: $TARGETDIR/riocore\" 20 60 12 \\" > ${TEMPFILE}2
+echo "	apt \"install rio dependencies\" ON \\" >> ${TEMPFILE}2
+if ! test -d riocore
 then
-	if ! whiptail --checklist "what do you want to install ?" 20 60 12 \
-	apt "install dependencies" ON \
-	rio "git clone riocore" ON \
-	icestorm "install OSS-Cad-Suite" ON \
-	gowin "install Gowin Toolchain" ON \
-	2> $TEMPFILE
-	then
-		rm -rf $TEMPFILE
-		doexit 0
-	fi
+	echo "	rio \"git clone riocore\" ON \\" >> ${TEMPFILE}2
 else
-	if ! whiptail --checklist "what do you want to install ?" 20 60 12 \
-	apt "install dependencies" ON \
-	rio "git clone riocore" ON \
-	icestorm "install OSS-Cad-Suite" ON \
-	2> $TEMPFILE
+	echo "	rio \"git clone riocore\" OFF \\" >> ${TEMPFILE}2
+fi
+if ! which linuxcnc >/dev/null
+then
+	echo "	linuxcnc \"install rio component loader\" OFF \\" >> ${TEMPFILE}2
+	echo "	halcompile \"install rio component loader\" OFF \\" >> ${TEMPFILE}2
+else
+	if ! test -e /usr/lib/linuxcnc/modules/rio.so
 	then
-		rm -rf $TEMPFILE
-		doexit 0
+		echo "	halcompile \"install rio component loader\" ON \\" >> ${TEMPFILE}2
+	else
+		echo "	halcompile \"install rio component loader\" OFF \\" >> ${TEMPFILE}2
 	fi
 fi
+if ! test -d riocore/toolchains/oss-cad-suite && ! which nextpnr-himbaechel >/dev/null
+then
+	echo "	icestorm \"install OSS-Cad-Suite\" ON \\" >> ${TEMPFILE}2
+else
+	echo "	icestorm \"install OSS-Cad-Suite\" OFF \\" >> ${TEMPFILE}2
+fi
+if test "$SYSTEM" = "x64"
+then
+	if ! test -d riocore/toolchains/gowin/IDE && ! which gw_ide >/dev/null
+	then
+		echo "	gowin \"install Gowin Toolchain\" ON \\" >> ${TEMPFILE}2
+	else
+		echo "	gowin \"install Gowin Toolchain\" OFF \\" >> ${TEMPFILE}2
+	fi
+fi
+echo "	probe_basic \"install Probe-Basic GUI\" OFF \\" >> ${TEMPFILE}2
+echo "	2> $TEMPFILE" >> ${TEMPFILE}2
 
 
+if ! bash ${TEMPFILE}2
+then
+	rm -rf $TEMPFILE
+	doexit 0
+fi
 
 if grep -s -q '"apt"' in $TEMPFILE
 then
+	if test "$SYSTEM" = "x64"
+	then
+		# removing unwanted package on amd/intel systems
+		sudo dpkg --purge raspi-firmware || true
+	fi
 	echo "installing dependencies"
 	sudo apt-get update || doexit 1
 	sudo apt-get -y install git python3 python3-pip python3-yaml python3-graphviz python3-pyqtgraph python3-pyqt5 python3-pyqt5.qtsvg python3-lxml || doexit 1
@@ -62,6 +93,17 @@ then
 		git clone https://github.com/multigcs/riocore.git || doexit 1
 	else
 		echo "riocore already installed"
+	fi
+fi
+
+if grep -s -q '"halcompile"' in $TEMPFILE
+then
+	if test -d riocore
+	then
+		echo "installing rio halcomponent loader"
+		sudo halcompile --install riocore/riocore/files/rio.c
+	else
+		echo "halcompile: no riocore found"
 	fi
 fi
 
@@ -120,12 +162,45 @@ then
 EOF
 fi
 
+if grep -s -q '"probe_basic"' in $TEMPFILE
+then
+	if whiptail --menu "installing ProbeBasic" 20 60 12 stable "Stable" develop "Develop" 2> ${TEMPFILE}2
+	then
+		PB_BRANCH=`cat ${TEMPFILE}2`
+		BASE_URL="https://repository.qtpyvcp.com/apt/pool/main/$PB_BRANCH"
+		LAST_HIYAPYCO=`wget -q -O- $BASE_URL/ | grep "python3-hiyapyco_" | grep "${SYSTEM2}\|all" | tail -n1 | head -n1 | cut -d'"' -f2`
+		LAST_PROBEBASIC=`wget -q -O- $BASE_URL/ | grep "python3-probe-basic_" | grep "${SYSTEM2}\|all" | tail -n1 | head -n1 | cut -d'"' -f2`
+		LAST_QTPYVCP=`wget -q -O- $BASE_URL/ | grep "python3-qtpyvcp_" | grep "${SYSTEM2}\|all" | tail -n1 | head -n1 | cut -d'"' -f2`
+		rm -f "./$LAST_HIYAPYCO" "./$LAST_PROBEBASIC" "./$LAST_QTPYVCP"
+		if ! wget "$BASE_URL/$LAST_HIYAPYCO"
+		then
+			echo "probe_basic: ERROR getting python3-hiyapyco: $BASE_URL/$LAST_HIYAPYCO"
+			rm -f "./$LAST_HIYAPYCO"
+		fi
+		if ! wget "$BASE_URL/$LAST_PROBEBASIC"
+		then
+			echo "probe_basic: ERROR getting python3-probe-basic: $BASE_URL/$LAST_PROBEBASIC"
+			rm -f "./$LAST_PROBEBASIC"
+		fi
+		if ! wget "$BASE_URL/$LAST_QTPYVCP"
+		then
+			echo "probe_basic: ERROR getting python3-qtpyvcp: $BASE_URL/$LAST_QTPYVCP"
+			rm -f "./$LAST_QTPYVCP"
+		fi
+		if test -e $LAST_HIYAPYCO && test -e $LAST_QTPYVCP && test -e $LAST_PROBEBASIC
+		then
+			sudo apt install -y debhelper-compat dh-python python4-setuptools python3-yaml python3-pyqt5.qtmultimedia python3-pyqt5.qtquick qml-module-qtquick-controls libqt5multimedia5-plugins python3-dev python3-docopt python3-qtpy python3-pyudev python3-psutil python3-markupsafe python3-vtk9 python3-pyqtgraph python3-simpleeval python3-jinja2 python3-deepdiff python3-sqlalchemy qttools5-dev-tools python3-serial python3-distro
+			sudo dpkg -i $LAST_HIYAPYCO $LAST_QTPYVCP $LAST_PROBEBASIC || sudo apt-get install -y -f
+		fi
+	fi
+fi
+
 echo ""
 echo "####################################################################"
 echo ""
 echo "# command examples:"
 echo ""
-echo "    cd ~/riocore/"
+echo "  cd $TARGETDIR/riocore/"
 echo ""
 echo "  # create new setup:"
 echo "    bin/rio-setup"
@@ -133,9 +208,12 @@ echo ""
 echo "  # generate, build and flash the Tangbob config"
 echo "    bin/rio-generator -b -f riocore/configs/Tangbob/config.json"
 echo ""
-echo "  # generate and start linuxcnc for the Tangbob config"
-echo "    bin/rio-generator -s riocore/configs/Tangbob/config.json"
-echo ""
+if ! which linuxcnc >/dev/null
+then
+	echo "  # generate and start linuxcnc for the Tangbob config"
+	echo "    bin/rio-generator -s riocore/configs/Tangbob/config.json"
+	echo ""
+fi
 echo "####################################################################"
 echo ""
 
