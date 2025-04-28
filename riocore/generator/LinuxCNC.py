13a75c04c72bab2f2cba50f55bc5e0c69961f8ca
Oliver <oliver@office-station.local>
2025-04-28 20:10:49 +0200

    little fixes

--- riocore/generator/LinuxCNC.py
+++ riocore/generator/LinuxCNC.py
@@ -1223,7 +1223,8 @@ class LinuxCNC:
                 vcp_sections.append(section)
         for plugin_instance in self.project.plugin_instances:
             for signal_name, signal_config in plugin_instance.signals().items():
-                if plugin_instance.plugin_setup.get("is_joint", False) and signal_name in {"position", "velocity", "position-cmd", "enable"}:
+                direction = signal_config["direction"]
+                if plugin_instance.plugin_setup.get("is_joint", False) and signal_name in {"position", "velocity", "position-cmd", "enable", "dty"}:
                     continue
                 userconfig = signal_config.get("userconfig", {})
                 displayconfig = userconfig.get("display", signal_config.get("display", {}))
@@ -1238,7 +1239,8 @@ class LinuxCNC:
         haltitles = {}
         for plugin_instance in self.project.plugin_instances:
             for signal_name, signal_config in plugin_instance.signals().items():
-                if plugin_instance.plugin_setup.get("is_joint", False) and signal_name in {"position", "velocity", "position-cmd", "enable"}:
+                direction = signal_config["direction"]
+                if plugin_instance.plugin_setup.get("is_joint", False) and signal_name in {"position", "velocity", "position-cmd", "enable", "dty"}:
                     continue
                 halname = signal_config["halname"]
                 prefix = ".".join(halname.split(".")[:-1])
@@ -1468,7 +1470,8 @@ class LinuxCNC:
 
             for plugin_instance in self.project.plugin_instances:
                 for signal_name, signal_config in plugin_instance.signals().items():
-                    if plugin_instance.plugin_setup.get("is_joint", False) and signal_name in {"position", "velocity", "position-cmd", "enable"}:
+                    direction = signal_config["direction"]
+                    if plugin_instance.plugin_setup.get("is_joint", False) and signal_name in {"position", "velocity", "position-cmd", "enable", "dty"}:
                         continue
                     vcp_add(tab, signal_config, "rio.")
 
