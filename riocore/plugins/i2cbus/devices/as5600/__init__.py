ccf3c24f374534b426c98a94dd259d14a9d26116
Oliver <oliver@office-station.local>
2025-05-03 22:13:48 +0200

    some fixes

--- riocore/plugins/i2cbus/devices/as5600/__init__.py
+++ riocore/plugins/i2cbus/devices/as5600/__init__.py
@@ -6,11 +6,13 @@ class i2c_device:
     }
 
     def __init__(self, setup, system_setup={}):
+        self.last = 0
+        self.revs = 0
         self.system_setup = system_setup
         self.name = setup["name"]
         self.addr = setup["address"]
         self.INTERFACE = {
-            f"{self.name}_val": {
+            f"{self.name}_angle": {
                 "size": 16,
                 "direction": "input",
             },
@@ -20,7 +22,11 @@ class i2c_device:
             },
         }
         self.SIGNALS = {
-            f"{self.name}_val": {
+            f"{self.name}_position": {
+                "direction": "input",
+                "format": "0.1f",
+            },
+            f"{self.name}_angle": {
                 "direction": "input",
                 "format": "0.1f",
             },
@@ -39,20 +45,47 @@ class i2c_device:
             },
             {
                 "mode": "read",
-                "var": f"{self.name}_val",
+                "var": f"{self.name}_angle",
                 "var_set": "{3'd0, data_in[11:0]}",
                 "bytes": 2,
             },
         ]
 
     def convert(self, signal_name, signal_setup, value):
-        if signal_name.endswith("_valid"):
-            return value
-        return value * 360 / 4096
+        if signal_name.endswith("_angle"):
+            new = value
+            diff = new - self.last
+            if diff < -2048:
+                self.revs += 1
+            elif diff > 2048:
+                self.revs -= 1
+            self.SIGNALS[f"{self.name}_position"]["value"] = (self.revs * 4096) + new
+            self.last = new
+
+            return value * 360 / 4096
+        return value
 
     def convert_c(self, signal_name, signal_setup):
-        if signal_name.endswith("_valid"):
-            return ""
-        return """
-        value = value * 360 / 4096;
-        """
+        if signal_name.endswith("_angle"):
+            varname = self.SIGNALS[f"{self.name}_position"]["varname"]
+            return f"""
+
+    static float revs = 0;
+    float diff = 0;
+
+    diff = raw_value - last_raw_value;
+
+    if (diff < -2048) {{
+        revs++;
+    }} else if (diff > 2048) {{
+        revs--;
+    }}
+
+    float position_value = (revs * 4096) + raw_value;
+    position_value = position_value + *data->{varname}_OFFSET;
+    position_value = position_value / *data->{varname}_SCALE;
+    *data->{varname} = position_value;
+
+    value = value * 360 / 4096;
+            """
+        return ""
