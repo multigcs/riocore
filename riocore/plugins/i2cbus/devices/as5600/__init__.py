52639062dd005af409ca7df0f39a7f49ed6bff17
Oliver <oliver@office-station.local>
2025-05-29 09:40:18 +0200

    as5600: rpm/rps calc

--- riocore/plugins/i2cbus/devices/as5600/__init__.py
+++ riocore/plugins/i2cbus/devices/as5600/__init__.py
@@ -1,3 +1,5 @@
+import time
+
 class i2c_device:
     options = {
         "info": "magnetic rotary position sensor",
@@ -25,6 +27,14 @@ class i2c_device:
             f"{self.name}_position": {
                 "direction": "input",
                 "format": "0.1f",
+            },
+            f"{self.name}_rps": {
+                "direction": "input",
+                "format": "0.2f",
+            },
+            f"{self.name}_rpm": {
+                "direction": "input",
+                "format": "0.2f",
             },
             f"{self.name}_angle": {
                 "direction": "input",
@@ -69,6 +79,7 @@ class i2c_device:
                 "bytes": 2,
             },
         ]
+        self.timer_last = time.time()
 
     def convert(self, signal_name, signal_setup, value):
         if signal_name.endswith("_angle"):
@@ -76,17 +87,30 @@ class i2c_device:
             diff = new - self.last
             if diff < -2048:
                 self.revs += 1
+                diff += 4096
             elif diff > 2048:
                 self.revs -= 1
+                diff -= 4096
             self.SIGNALS[f"{self.name}_position"]["value"] = (self.revs * 4096) + new
             self.last = new
 
+            # calc rps/rpm
+            timer_new = time.time()
+            timer_diff = timer_new - self.timer_last
+            rps = diff / timer_diff / 4096
+            self.timer_last = timer_new
+            self.SIGNALS[f"{self.name}_rps"]["value"] = rps
+            self.SIGNALS[f"{self.name}_rpm"]["value"] = rps * 60
+
+            # calc angle
             return value * 360 / 4096
         return value
 
     def convert_c(self, signal_name, signal_setup):
         if signal_name.endswith("_angle"):
             varname = self.SIGNALS[f"{self.name}_position"]["varname"]
+            varname_rps = self.SIGNALS[f"{self.name}_rps"]["varname"]
+            varname_rpm = self.SIGNALS[f"{self.name}_rpm"]["varname"]
             return f"""
 
     static float revs = 0;
@@ -96,15 +120,37 @@ class i2c_device:
 
     if (diff < -2048) {{
         revs++;
+        diff += 4096;
     }} else if (diff > 2048) {{
         revs--;
+        diff -= 4096;
     }}
 
+    // calc rps/rpm
+    static uint8_t pcnt = 0;
+    static float last_rpssum = 0;
+    static float diff_sum = 0;
+    static float duration_sum = 0.0;
+    diff_sum += diff;
+    duration_sum += *data->duration;
+    pcnt++;
+    if (pcnt == 100) {{
+        last_rpssum = diff_sum / duration_sum / 4096;
+        pcnt = 0;
+        duration_sum = 0;
+        diff_sum = 0;
+    }}
+    *data->{varname_rpm} = last_rpssum;
+    *data->{varname_rps} = last_rpssum * 60;
+
+
+    // calc position
     float position_value = (revs * 4096) + raw_value;
     position_value = position_value + *data->{varname}_OFFSET;
     position_value = position_value / *data->{varname}_SCALE;
     *data->{varname} = position_value;
 
+    // calc angle
     value = value * 360 / 4096;
             """
         return ""
