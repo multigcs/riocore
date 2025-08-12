ebec26369370f39b71b08b88bbfa3a405714b7df
odippel@ypsilon.net <odippel@odippel.isys.loc>
2025-08-12 12:12:28 +0200

    ise: readme

--- riocore/generator/toolchains/ise/README.md
+++ riocore/generator/toolchains/ise/README.md
@@ -3,3 +3,13 @@
 
 * URL: [https://www.xilinx.com/support/download/index.html/content/xilinx/en/downloadNav/vivado-design-tools/archive-ise.html](https://www.xilinx.com/support/download/index.html/content/xilinx/en/downloadNav/vivado-design-tools/archive-ise.html)
 
+
+```
+# mkdir /tmp/ise-inst
+# cd /tmp/ise-inst
+# tar xvpf ~/Downloads/Xilinx_ISE_DS_Lin_14.7_1015_1.tar
+# sudo ln -s /usr/lib/x86_64-linux-gnu/libncurses.so.6 /usr/lib/x86_64-linux-gnu/libncurses.so.5
+# ./xsetup
+```
+
+
--- riocore/generator/toolchains/ise/toolchain.py
+++ riocore/generator/toolchains/ise/toolchain.py
@@ -17,7 +17,15 @@ class Toolchain:
         info = {
             "url": "https://www.xilinx.com/support/download/index.html/content/xilinx/en/downloadNav/vivado-design-tools/archive-ise.html",
             "info": "Xilinx/AMD ISE WebPACK",
-            "description": "",
+            "description": """
+```
+# mkdir /tmp/ise-inst
+# cd /tmp/ise-inst
+# tar xvpf ~/Downloads/Xilinx_ISE_DS_Lin_14.7_1015_1.tar
+# sudo ln -s /usr/lib/x86_64-linux-gnu/libncurses.so.6 /usr/lib/x86_64-linux-gnu/libncurses.so.5
+# ./xsetup
+```
+""",
         }
         return info
 
