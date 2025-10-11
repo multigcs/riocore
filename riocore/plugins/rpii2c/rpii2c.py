89857f771d023513221ead16340c82a0959a549b
Oliver <oliver@office-station.local>
2025-10-11 20:53:30 +0200

    rpii2c: small fixes

--- riocore/plugins/rpii2c/rpii2c.py
+++ riocore/plugins/rpii2c/rpii2c.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 #
 # loadusr -Wn rpii2c ./rpii2c.py PREFIX TYPE ADDR CFG [PREFIX TYPE ADDR CFG [...]]
-# loadusr -Wn rpii2c ./rpii2c.py rpii2c0 pcf8574 0x20 00000000 rpii2c1 pcf8574 0x21 11111111 rpii2c2 ads1115 0x48 0
+# loadusr -Wn rpii2c ./rpii2c.py  io0 pcf8574 0x20 00000000  io1 pcf8574 0x21 11111111  adc0 ads1115 0x48 0
 #  pcf8574 CFG: 8bit invert-mask
 #  ads1115 CFG: unused
+#  lm75    CFG: unused
 #
 
 
@@ -27,18 +28,18 @@ ads1115_setupRegister2 = [
 ]
 
 
-expanders = []
+configs = []
 for an in range((len(sys.argv) - 1) // 4):
     instance = sys.argv[an * 4 + 1]
     device = sys.argv[an * 4 + 2]
     addr = sys.argv[an * 4 + 3]
     invert_mask = sys.argv[an * 4 + 4]
-    expanders.append((instance, device, addr, invert_mask))
+    configs.append((instance, device, int(addr[2:], 16), invert_mask))
 
 
 h = hal.component("rpii2c")
-for expander in expanders:
-    instance, device, addr, invert_mask = expander
+for config in configs:
+    instance, device, addr, invert_mask = config
     if device == "pcf8574":
         for pn in range(8):
             h.newpin(f"{instance}.p{pn:02d}-in", hal.HAL_BIT, hal.HAL_OUT)
@@ -56,17 +57,17 @@ h.ready()
 bus = smbus.SMBus(1)
 
 while True:
-    for expander in expanders:
-        instance, device, addr, invert_mask = expander
+    for config in configs:
+        instance, device, addr, invert_mask = config
         try:
             if device == "lm75":
-                bus.write_byte_data(int(addr[2:], 16), 0x01, 0x00)
+                bus.write_byte_data(addr, 0x01, 0x00)
 
         except Exception as error:
-            print(f"ERROR: expander {expander}: {error}")
+            print(f"ERROR: device init {device} @ 0x{addr:02x}: {error}")
 
-    for expander in expanders:
-        instance, device, addr, invert_mask = expander
+    for config in configs:
+        instance, device, addr, invert_mask = config
         try:
             if device == "pcf8574":
                 out_bits = ""
@@ -80,9 +81,9 @@ while True:
 
                 out_data = int(out_bits, 2)
                 # print(addr[2:], out_bits, out_data)
-                bus.write_byte(int(addr[2:], 16), out_data)
+                bus.write_byte(addr, out_data)
 
-                data = bus.read_byte(int(addr[2:], 16))
+                data = bus.read_byte(addr)
                 data = f"{data:08b}"
                 for pn in range(8):
                     value = False
@@ -94,7 +95,7 @@ while True:
                     h[f"{instance}.p{pn:02d}-in-not"] = not value
 
             elif device == "lm75":
-                data = bus.read_i2c_block_data(int(addr[2:], 16), 0x00, 2)
+                data = bus.read_i2c_block_data(addr, 0x00, 2)
                 cTemp = (data[0] * 256 + (data[1] & 0x80)) / 128
                 if cTemp > 255:
                     cTemp -= 512
@@ -106,18 +107,18 @@ while True:
                     ads1115_setupRegister2[1] = f"{4 + an:03b}"
                     reg1 = int("".join(ads1115_setupRegister1), 2)
                     reg2 = int("".join(ads1115_setupRegister2), 2)
-                    bus.write_i2c_block_data(int(addr[2:], 16), 1, [reg2, reg1])
+                    bus.write_i2c_block_data(addr, 1, [reg2, reg1])
                     for rn in range(5):
-                        data = bus.read_i2c_block_data(int(addr[2:], 16), 1, 1)
+                        data = bus.read_i2c_block_data(addr, 1, 1)
                         if (data[0] & (1 << 7)) != 0:
                             break
                         time.sleep(0.01)
-                    data = bus.read_i2c_block_data(int(addr[2:], 16), 0, 2)
+                    data = bus.read_i2c_block_data(addr, 0, 2)
                     value = (data[0] << 8) + data[1]
                     value = (value >> 3) / 1000.0
                     h[f"{instance}.adc{an}"] = value
 
         except Exception as error:
-            print(f"ERROR: expander {expander}: {error}")
+            print(f"ERROR: device read {device} @ 0x{addr:02x}: {error}")
 
     time.sleep(0.01)
