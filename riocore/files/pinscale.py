71d0adaa2312c349e4d4e3e0b489ebe879e90a15
Oliver <oliver@office-station.local>
2025-10-25 13:24:24 +0200

    little fixes

--- riocore/files/pinscale.py
+++ riocore/files/pinscale.py
@@ -4,8 +4,8 @@ import json
 
 json_file = sys.argv[1]
 slot_selection = sys.argv[2]
-xoff = int(sys.argv[3])
-yoff = int(sys.argv[4])
+xscale = float(sys.argv[3])
+yscale = float(sys.argv[4])
 
 jdata = json.loads(open(json_file, "r").read())
 
@@ -16,12 +16,21 @@ if slot_selection == "main":
 
 else:
     found = False
+    min_x = 999999999999
+    min_y = 999999999999
     for slot in jdata["slots"]:
         if slot_selection == slot["name"]:
             found = True
             for pin, pdata in slot["pins"].items():
-                pdata["pos"][0] += xoff
-                pdata["pos"][1] += yoff
+                min_x = min(min_x, pdata["pos"][0])
+                min_y = min(min_x, pdata["pos"][1])
+
+
+    for slot in jdata["slots"]:
+        if slot_selection == slot["name"]:
+            for pin, pdata in slot["pins"].items():
+                pdata["pos"][0] = (pdata["pos"][0] - min_x) * xscale + min_x
+                pdata["pos"][1] = (pdata["pos"][1] - min_y) * yscale + min_y
 
     if not found:
         print(f"slot '{slot_selection}' not found")
