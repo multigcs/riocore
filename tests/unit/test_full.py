fc44e441677396a04054f2005d80ba31027dee91
Oliver <oliver@office-station.local>
2025-11-02 10:13:29 +0100

    more tests

---
+++ tests/unit/test_full.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+#
+#
+
+import glob
+import os
+import pytest
+import riocore
+import os.path
+import shutil
+
+
+def test_generator():
+
+    for config in glob.glob("tests/unit/data/full/*.json"):
+        print(config)
+        target = config.split("/")[-1].replace(".json", "")
+
+        if os.path.exists("tests/unit/output"):
+            shutil.rmtree("tests/unit/output")
+        project = riocore.Project(config, "tests/unit/output")
+        project.generator(True)
+
+        if not os.path.exists(f"tests/unit/output/{target}/LinuxCNC/rio.hal"):
+            assert False
+
+
+        if not os.path.exists(f"tests/unit/data/full/{target}"):
+            shutil.copytree(f"tests/unit/output/{target}", f"tests/unit/data/full/{target}")
+
+        ret = os.system(f"diff -r tests/unit/data/full/{target}/ tests/unit/output/{target}/")
+        assert ret == 0
+
