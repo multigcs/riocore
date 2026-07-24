25234ea9581dff463acb31862bbdc7571ed56b31
odippel@ypsilon.net <odippel@odippel.isys.loc>
2026-07-24 13:24:24 +0200

    serv: fixing gpio out

--- riocore/plugins/serv/src/main.c
+++ riocore/plugins/serv/src/main.c
@@ -1,19 +1,35 @@
 
+#include <stdint.h>
+
 #define GPIO_BASE ((volatile unsigned int *) 0x100)
 #define DELAY 0x20000
 
+#define INPUT  0
+#define OUTPUT 1
+#define LOW    0
+#define HIGH   1
+#define TOGGLE 2
+
+// GPIO functions
+void digitalWrite(uint8_t num, uint8_t value) {
+    if (value == HIGH) {
+        *GPIO_BASE |= (1<<num);
+    } else if (value == LOW) {
+        *GPIO_BASE &= ~(1<<num);
+    } else if (TOGGLE) {
+        if ((*GPIO_BASE & (1<<num)) != 0) {
+            *GPIO_BASE &= ~(1<<num);
+        } else {
+            *GPIO_BASE |= (1<<num);
+        }
+    }
+}
+
 int main() {
-
-    *GPIO_BASE = 1;
-
+    digitalWrite(1, HIGH);
     while (1) {
 
-        *GPIO_BASE = 0;
-        for (int i = 0; i < DELAY; i++) {
-            asm("nop");
-        }
-
-        *GPIO_BASE = 1;
+        digitalWrite(0, TOGGLE);
         for (int i = 0; i < DELAY; i++) {
             asm("nop");
         }
