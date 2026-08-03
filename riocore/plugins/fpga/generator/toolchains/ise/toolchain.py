dd8a57d1bd490ea4ff70384070608f4e301940dd
odippel@ypsilon.net <odippel@odippel.isys.loc>
2026-08-03 13:39:32 +0200

    board: initial support for Core3S250E

--- riocore/plugins/fpga/generator/toolchains/ise/toolchain.py
+++ riocore/plugins/fpga/generator/toolchains/ise/toolchain.py
@@ -18,14 +18,16 @@ class Toolchain:
     @classmethod
     def info(cls):
         return {
-            "url": "https://www.xilinx.com/support/download/index.html/content/xilinx/en/downloadNav/vivado-design-tools/archive-ise.html",
-            "info": "Xilinx/AMD ISE WebPACK",
+            "_url": "https://www.xilinx.com/support/download/index.html/content/xilinx/en/downloadNav/vivado-design-tools/archive-ise.html",
+            "url": "https://www.amd.com/en/support/downloads/adaptive-socs-and-fpgas/legacy-ise/v2012_4---14_7.html",
+            "info": "Xilinx/AMD ISE WebPACK - Full Installer for Linux",
             "description": """
 ```
 # mkdir /tmp/ise-inst
 # cd /tmp/ise-inst
 # tar xvpf ~/Downloads/Xilinx_ISE_DS_Lin_14.7_1015_1.tar
 # sudo ln -s /usr/lib/x86_64-linux-gnu/libncurses.so.6 /usr/lib/x86_64-linux-gnu/libncurses.so.5
+# cd Xilinx_ISE_DS_Lin_14.7_1015_1/
 # ./xsetup
 ```
 """,
