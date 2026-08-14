060e9cd115ffb3cb867401403658b205269141fe
odippel@ypsilon.net <odippel@odippel.isys.loc>
2026-08-14 15:03:36 +0200

    some fixes

--- riocore/files/scale-board.py
+++ riocore/files/scale-board.py
@@ -61,13 +61,11 @@ if btype == "fpga":
                 print(pin, pos)
                 data["pos"] = [int(pos[0] * xscale), int(pos[1] * yscale)]
 
-    for slot in jdata.get("plugins", []):
-        for pin, data in slot["pins"].items():
-            pos = data.get("pos")
-            if pos:
-                print(pin, pos)
-                data["pos"] = [int(pos[0] * xscale), int(pos[1] * yscale)]
-
+    for plugin in jdata.get("plugins", []):
+        rpos = plugin.get("rpos")
+        if rpos:
+            print(rpos)
+            plugin["rpos"] = [int(rpos[0] * xscale), int(rpos[1] * yscale)]
 
 elif btype == "breakout":
     for slot in jdata.get("slots", []):
