0f9759ac575d8bc3f7ad3264e71caec0ff2066b1
odippel@ypsilon.net <odippel@odippel.isys.loc>
2026-08-14 11:53:26 +0200

    better image/pin rescale helper

--- riocore/files/scale-board.py
+++ riocore/files/scale-board.py
@@ -3,19 +3,34 @@ import os
 import subprocess
 import sys
 
-btype = "fpga"
-# btype = "mesa"
-# btype = "ninja"
-# btype = "breakout"
-# btype = "satmcu"
-# btype = "sserial"
-
-board_name = sys.argv[1]
-json_file = f"riocore/plugins/{btype}/boards/{board_name}.json"
-png_file = f"riocore/plugins/{btype}/boards/{board_name}.png"
-
+filename = sys.argv[1]
 xscale = float(sys.argv[2])
 yscale = float(sys.argv[3])
+
+board_name = filename.split("/")[4].split(".", 1)[0]
+btype = filename.split("/")[2]
+
+
+if btype == "i2c_device":
+    print("image rescale only")
+    png_file = f"riocore/plugins/{btype}/boards/{board_name}.png"
+    res = subprocess.check_output(["identify", png_file]).decode()
+    size = res.split()[2].split("x")
+    size_x = int(size[0])
+    size_y = int(size[1])
+    cmd = f"convert -scale {int(size_x * xscale)}x {png_file} {png_file}"
+    print(cmd)
+    os.system(cmd)
+    exit(0)
+
+
+if btype == "ethercat":
+    json_file = f"riocore/plugins/{btype}/modules/{board_name}.json"
+    png_file = f"riocore/plugins/{btype}/modules/{board_name}.png"
+else:
+    json_file = f"riocore/plugins/{btype}/boards/{board_name}.json"
+    png_file = f"riocore/plugins/{btype}/boards/{board_name}.png"
+
 
 jdata = json.loads(open(json_file, "r").read())
 
@@ -44,6 +59,14 @@ elif btype == "ninja":
         if pos:
             print(pin, pos)
             data["pos"] = [int(pos[0] * xscale), int(pos[1] * yscale)]
+
+elif btype == "ninja":
+    for pin, data in jdata["pins"].items():
+        pos = data.get("pos")
+        if pos:
+            print(pin, pos)
+            data["pos"] = [int(pos[0] * xscale), int(pos[1] * yscale)]
+
 else:
     for pin, data in jdata["pins"].items():
         pos = data.get("pos")
